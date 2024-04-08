de75483bdbe3816c38675aac2ba75b987e454f82
elizabethmiranda <mirandaeli23@gmail.com>
2024-04-07 23:10:33 -0600

    Iniciando proyecto

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\CategoriaController;
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('categorias',CategoriaController::class);
+
